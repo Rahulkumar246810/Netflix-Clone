c7dd159fb0bb8ac54f493efeff10e0093b61df78
Rahulkumar246810 <rahulkumar246810@gmail.com>
2022-11-04 10:31:06 +0530

    Netflix-Clone

---
+++ script.js
@@ -0,0 +1,107 @@
+// Question-1
+const question1=document.getElementById('answer1');
+const img1 = document.getElementById('img1');
+const img2 = document.getElementById('img2');
+const btn1 = document.getElementById('question1');
+btn1.onclick = function(){
+    if(question1.style.display === "none"){
+        question1.style.display = "block";
+        img1.style.display = "none";
+        img2.style.display = "block";
+    }
+    else{
+        question1.style.display = "none";
+        img1.style.display = "block";
+        img2.style.display = "none";
+    }
+}
+
+// Question-2
+const question2=document.getElementById('answer2');
+const img3 =document.getElementById('img3');
+const img4 = document.getElementById('img4');
+const btn2 =document.getElementById('question2');
+btn2.onclick = function(){
+    if(question2.style.display === "none"){
+        question2.style.display = "block";
+        img3.style.display = "none";
+        img4.style.display = "block";
+    }
+    else{
+        question2.style.display = "none";
+        img3.style.display = "block";
+        img4.style.display = "none";
+    }
+}
+
+// Question-3
+const question3=document.getElementById('answer3');
+const img5 =document.getElementById('img5');
+const img6 = document.getElementById('img6');
+const btn3 =document.getElementById('question3');
+btn3.onclick = function(){
+    if(question3.style.display === "none"){
+        question3.style.display = "block";
+        img5.style.display = "none";
+        img6.style.display = "block";
+    }
+    else{
+        question3.style.display = "none";
+        img5.style.display = "block";
+        img6.style.display = "none";
+    }
+}
+
+// Question-4
+const question4=document.getElementById('answer4');
+const img7 =document.getElementById('img7');
+const img8 = document.getElementById('img8');
+const btn4 =document.getElementById('question4');
+btn4.onclick = function(){
+    if(question4.style.display === "none"){
+        question4.style.display = "block";
+        img7.style.display = "none";
+        img8.style.display = "block";
+    }
+    else{
+        question4.style.display = "none";
+        img7.style.display = "block";
+        img8.style.display = "none";
+    }
+}
+
+// Question-5
+const question5=document.getElementById('answer5');
+const img9 =document.getElementById('img9');
+const img10 = document.getElementById('img10');
+const btn5 =document.getElementById('question5');
+btn5.onclick = function(){
+    if(question5.style.display === "none"){
+        question5.style.display = "block";
+        img9.style.display = "none";
+        img10.style.display = "block";
+    }
+    else{
+        question5.style.display = "none";
+        img9.style.display = "block";
+        img10.style.display = "none";
+    }
+}
+
+// Question-6
+const question6=document.getElementById('answer6');
+const img11 =document.getElementById('img11');
+const img12 = document.getElementById('img12');
+const btn6 =document.getElementById('question6');
+btn6.onclick = function(){
+    if(question6.style.display === "none"){
+        question6.style.display = "block";
+        img11.style.display = "none";
+        img12.style.display = "block";
+    }
+    else{
+        question6.style.display = "none";
+        img11.style.display = "block";
+        img12.style.display = "none";
+    }
+}
